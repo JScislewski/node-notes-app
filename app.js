668db3087a5c2f76762da5a45747156e444c36ab
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-07-16 01:48:01 +0200

    Update adding note

--- app.js
+++ app.js
@@ -1,37 +1,51 @@
 const chalk = require("chalk");
 const yargs = require("yargs");
+const { argv } = require("yargs");
 
 yargs
   .command({
     command: "add",
-    describe: "Adding command",
-    handler: function () {
-      console.log(chalk.green("Adding a note"));
-    },
-  })
-  .command({
-    command: "remove",
-    describe: "removing command",
-    handler: function () {
-      console.log(chalk.red("Removing a note"));
-    },
-  });
-yargs.parse();
-
-/* yargs
-  .command({
-    command: "add",
     describe: "Add new note",
-    hander: function () {
-      console.log(chalk.red("Removing note"));
+    builder: {
+      title: {
+        describe: "Note title",
+        demandOption: true,
+        type: "string",
+      },
+      content: {
+        describe: "Note content",
+        demandOption: true,
+        type: "string",
+      },
+    },
+    handler: function () {
+      console.log(
+        chalk.green("Adding new note\n--------------\n"),
+        chalk.blue(`Title: ${argv.title}`),
+        "\n",
+        chalk.italic.cyan(`Content: ${argv.content}`)
+      );
     },
   })
   .command({
     command: "remove",
     describe: "Remove a note",
-    hander: function () {
-      console.log(chalk.red("Removing note"));
+    handler: function () {
+      console.log(chalk.red("Removing a note"));
+    },
+  })
+  .command({
+    command: "list",
+    describe: "List all notes",
+    handler: function () {
+      console.log(chalk.blue("List all notes"));
+    },
+  })
+  .command({
+    command: "read",
+    describe: "Read note",
+    handler: function () {
+      console.log(chalk.blue("Read a note"));
     },
   });
-
-yargs.parse(); */
+yargs.parse();
