ff5ccab3cdd74524d56846364a2fec6cc759ff78
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-07-16 18:08:11 +0200

    Add loading notes from notes.js

--- notes.js
+++ notes.js
@@ -8,18 +8,29 @@ const addNote = ({ title, content }) => {
     "\n",
     chalk.italic.cyan(`Content: ${content}`)
   );
-  const note = {
+  const notes = loadNotes();
+  notes.push({
     title,
     content,
-  };
-  const noteJSON = JSON.stringify(note);
-  fs.writeFileSync("notes.json", noteJSON);
+  });
+  saveNotes(notes);
+};
+
+const saveNotes = (notes) => {
+  const notesJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", notesJSON);
 };
 
 const loadNotes = () => {
-  const dataBuffer = fs.readFileSync("notes.json");
-  const dataJSON = dataBuffer.toString();
-  return JSON.parse(dataJSON);
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    console.log(JSON.parse(dataJSON));
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    console.log(chalk.red("There are no notes"));
+    return [];
+  }
 };
 
 module.exports = {
